8560a55d41d9a52958dc3cf0548ed49286f7a0f3
luphord <luphord@protonmail.com>
2015-11-06 17:18:38 +0000

    refactoring: extracted Cell into separate js file

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,4 @@
-var arithmepad = (function(ace, $) {
-  
-  var classes = {
-    input: 'arithmepad-input',
-    output: 'arithmepad-output',
-    cell: 'arithmepad-cell',
-    markdown: 'arithmepad-markdown',
-    editSelection: 'arithmepad-edit-selection',
-    commandSelection: 'arithmepad-command-selection'
-  };
+arithmepad = (function(ace, $, Cell, classes) {
   
   var div = {};
   _(classes).each(function(cls, clsName) {
@@ -16,101 +7,7 @@ var arithmepad = (function(ace, $) {
     };
   });
   
-  // Cell object creation
-  
-  var Cell = function(domNode) {
-    if (typeof domNode === 'undefined') {
-      throw 'A DOM node is required for a cell';
-    }
-    this.$node = $(domNode);
-    if (this.$node.length != 1) {
-      throw 'Cannot create cell: domNode has a length of ' + this.$node.length;
-    }
-  };
-  
-  Cell.fromEditor = function(editor) {
-    return new Cell($(editor.container).parent());
-  };
-  
-  // Cell components (input/output)
-  
-  Cell.prototype.getInput = function() {
-    return this.$node.find('.' + classes.input);
-  };
-  
-  Cell.prototype.getEditor = function() {
-    var sel = this.$node.find('.' + classes.input);
-    if (sel.length > 0) {
-      return ace.edit(sel[0]);
-    }
-  };
-  
-  // Cell editor options
-  
-  Cell.editorOptions = {
-    mode: "ace/mode/javascript",
-    tabSize: 2,
-    //theme: theme,
-    showGutter: false,
-    maxLines: 30,
-    autoScrollEditorIntoView: true
-  };
-  
-  Cell.prototype.getAceOptions = function() {
-    var options = _.clone(Cell.editorOptions);
-    if (this.$node.hasClass(classes.markdown)) {
-      options.mode = 'ace/mode/markdown';
-    }
-    return options;
-  };
-  
-  // Cell navigation (previous/next, scrolling)
-  
-  Cell.prototype.getPrevious = function() {
-    var previous = this.$node.prevAll('.' + classes.cell);
-    if (previous.length > 0) {
-      return new Cell(previous[0]);
-    }
-  };
-  
-  Cell.prototype.getNext = function() {
-    var next = this.$node.nextAll('.' + classes.cell);
-    if (next.length > 0) {
-      return new Cell(next[0]);
-    }
-  };
-  
-  Cell.prototype.scrollUpTo = function() {
-    this.$node[0].scrollIntoView(true);
-    window.scrollBy(0, -90);
-  };
-  
-  Cell.prototype.scrollDownTo = function() {
-    this.$node[0].scrollIntoView(false);
-  };
-  
-  // Cell selection
-  
-  Cell.emptyEditAndCommandSelection = function() {
-    $('.' + classes.editSelection).removeClass(classes.editSelection);
-    $('.' + classes.commandSelection).removeClass(classes.commandSelection);
-  };
-  
-  Cell.prototype.selectInCommandMode = function() {
-    Cell.emptyEditAndCommandSelection();
-    this.$node.addClass(classes.commandSelection);
-  };
-  
-  // Cell set/insert content
-  
-  Cell.prototype.setResult = function(result, setHtml) {
-    var output = this.$node.find('.' + classes.output);
-    if (setHtml) {
-      output.html(result);
-    } else {
-      output.text(result);
-    }
-  };
+  // Cell functions
   
   Cell.prototype.insertEditorAndOutput = function(code, result) {
     var input = div.input();
@@ -450,4 +347,4 @@ var arithmepad = (function(ace, $) {
       classes: classes
     }
   }
-})(ace, jQuery);
+})(ace, jQuery, arithmepad.Cell, arithmepad.__.classes);
